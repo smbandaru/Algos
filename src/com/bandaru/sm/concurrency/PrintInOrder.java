2915c94b674a5310e23614156dd13dcade8c6bd0
smbandaru <9829911+smbandaru@users.noreply.github.com>
2021-06-22 22:02:02 +0530

    0622 commit

---
+++ src/com/bandaru/sm/concurrency/PrintInOrder.java
@@ -0,0 +1,73 @@
+package com.bandaru.sm.concurrency;
+
+/**
+ * Suppose we have a class:
+ * <p>
+ * public class Foo {
+ * public void first() { print("first"); }
+ * public void second() { print("second"); }
+ * public void third() { print("third"); }
+ * }
+ * <p>
+ * The same instance of Foo will be passed to three different threads. Thread A will call first(), thread B will call second(), and thread C will call third(). Design a mechanism and modify the program to ensure that second() is executed after first(), and third() is executed after second().
+ * <p>
+ * Note:
+ * <p>
+ * We do not know how the threads will be scheduled in the operating system, even though the numbers in the input seem to imply the ordering. The input format you see is mainly to ensure our tests' comprehensiveness.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,2,3]
+ * Output: "firstsecondthird"
+ * Explanation: There are three threads being fired asynchronously. The input [1,2,3] means thread A calls first(), thread B calls second(), and thread C calls third(). "firstsecondthird" is the correct output.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: nums = [1,3,2]
+ * Output: "firstsecondthird"
+ * Explanation: The input [1,3,2] means thread A calls first(), thread B calls third(), and thread C calls second(). "firstsecondthird" is the correct output.
+ */
+public class PrintInOrder {
+    int i;
+
+    public PrintInOrder() {
+        i = 1;
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        synchronized (this) {
+            while (i != 1)
+                wait();
+            printFirst.run();
+            i++;
+            notifyAll();
+        }
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        synchronized (this) {
+            while (i != 2)
+                wait();
+            printSecond.run();
+            i++;
+            notifyAll();
+        }
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+
+        // printThird.run() outputs "third". Do not change or remove this line.
+        synchronized (this) {
+            while (i != 3)
+                wait();
+            printThird.run();
+            notifyAll();
+        }
+    }
+}
